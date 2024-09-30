a03f656a476fa8672fde363611c47a53d8a45bb3
JJJHolscher <jochem.holscher@gmail.com>
2024-09-30 15:06:35 +0200

    node2nix debugging

--- default.nix
+++ default.nix
@@ -2,25 +2,20 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  inherit (pkgs) fetchFromGitHub;
+  inherit (pkgs) fetchFromGitHub nodejs;
   inherit (pkgs.lib) licenses;
 
-  # Node packages
-  nodePackages = pkgs.nodePackages;
-
-  # Translation Server Version
-  translationServerVersion = "c4f03e78e1c50dc61a2b8e5a452284581d9ad0e3"; # Specific commit for reproducibility
+  # Import the generated Node.js environment
+  nodeEnv = import ./node-env.nix { inherit pkgs; };
 
   # Translation Server Package
-  translation-server = nodePackages.buildNodePackage rec {
-    pname = "translation-server";
-    version = translationServerVersion;
-
+  translation-server = nodeEnv.buildNodePackage rec {
+    name = "translation-server";
     src = fetchFromGitHub {
       owner = "zotero";
       repo = "translation-server";
-      rev = translationServerVersion;
-      sha256 =  "sha256-w60KHZsrg+0Cm6zd7Z5t+OZy/p+HWvTQokXQEKBXWow="; # Replace with the correct hash
+      rev = "c4f03e78e1c50dc61a2b8e5a452284581d9ad0e3";
+      sha256 = "sha256-w60KHZsrg+0Cm6zd7Z5t+OZy/p+HWvTQokXQEKBXWow=";  # Replace with the correct hash
     };
 
     # Replace SSH URLs with HTTPS in package.json
@@ -29,29 +24,36 @@ let
         --replace "git+ssh://git@github.com/" "git+https://github.com/"
     '';
 
-    # Fix the shebang line in translation-server.js
-    postInstall = ''
-      substituteInPlace $out/translation-server.js \
-        --replace "#!/usr/bin/env node" "#!${pkgs.nodejs}/bin/node"
+    # Build the package
+    buildPhase = ''
+      runHook preBuild
+      npm run build
+      runHook postBuild
     '';
 
-    # Expose the binary
-    pkgBin = "translation-server.js";
+    # Fix the shebang line
+    postInstall = ''
+      substituteInPlace $out/bin/translation-server \
+        --replace "#!/usr/bin/env node" "#!${nodejs}/bin/node"
+    '';
 
     meta = with pkgs.lib; {
       description = "Zotero Translation Server";
       homepage = "https://github.com/zotero/translation-server";
       license = licenses.mit;
-      platforms = platforms.unix;
+      platforms = platforms.all;
     };
   };
 
-  # Python Script Package
+  # Python Script Package (unchanged)
   readwise-to-zotero = pkgs.python3Packages.buildPythonApplication {
     pname = "readwise-to-zotero";
     version = "0.1.0";
 
-    src = ./.;
+    src = pkgs.lib.cleanSourceWith {
+      src = ./.;
+      filter = (path: type: true);  # Include all files
+    };
 
     propagatedBuildInputs = with pkgs.python3Packages; [ requests toml ];
 
@@ -66,7 +68,7 @@ let
       description = "CLI tool to import Readwise data into Zotero";
       homepage = "https://github.com/yourusername/readwise-to-zotero";
       license = licenses.mit;
-      platforms = platforms.unix;
+      platforms = platforms.all;
     };
   };
 
@@ -75,6 +77,12 @@ in
 # Final Package Combining Both
 pkgs.stdenv.mkDerivation {
   name = "readwise-to-zotero-with-translation-server";
+
+  # Specify that there is no source
+  src = null;
+
+  # Override the unpackPhase to skip it
+  unpackPhase = ":";
 
   buildInputs = [ translation-server readwise-to-zotero ];
 
@@ -108,6 +116,6 @@ pkgs.stdenv.mkDerivation {
     description = "CLI tool to import Readwise data into Zotero, includes the translation server";
     homepage = "https://github.com/yourusername/readwise-to-zotero";
     license = licenses.mit;
-    platforms = platforms.unix;
+    platforms = platforms.all;
   };
 }
